607615ecaef6766f5f4fdc94be471cea21dc97f2
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-22 18:24:56 +0300

    [:getter_api] Impl | Intermediate: cpp-file generation

--- getter_api.py
+++ getter_api.py
@@ -1,6 +1,7 @@
 import common
 
 HEADER_PREFIX = "MosseApi.hpp"
+CPP_PREFIX = "MosseApi.cpp"
 _NL = "\n"
 _DNL = "\n\n"
 
@@ -17,10 +18,30 @@ def _header_generate_iter():
 	yield define_after
 
 
+def _cpp_generate_iter():
+	yield '#include "GetterApi.hpp"' + _DNL
+
+	make_window_size_pair = lambda rows, cols: "{%d, %d}" % (rows, cols)
+	map_window_size_pair = map(lambda win: make_window_size_pair(*win), common.WINDOWS)
+	window_size_pairs = ',\n\t'.join(map_window_size_pair)
+
+	yield """\
+static constexpr unsigned kWindowSizes[][2] = {
+	%s
+};
+""" % window_size_pairs
+
+
 def _header_generate():
 	return ''.join(_header_generate_iter())
+
+
+def _cpp_generate():
+	return ''.join(_cpp_generate_iter())
 
 
 def generate_format_savefile():
 	generated = _header_generate()
 	common.append_file(generated, HEADER_PREFIX)
+	generated = _cpp_generate()
+	common.append_file(generated, CPP_PREFIX)
