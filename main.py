c1af53d6ff04d98258e22e425e0ef31312f1481a
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-22 16:29:31 +0300

    [:main] Impl | Separate generation

--- main.py
+++ main.py
@@ -5,15 +5,43 @@ import hann
 
 
 GAUSS_KERNEL_GENERATE = False
+HANN_PREFIX = "Hann"
+GAUSS_PREFIX = "GaussKernel"
+GAUSS_FFT_PREFIX = "GaussKernelFft"
+WINDOW_SIZES_ROWS_COLS = common.WINDOWS
+
+
+def _make_filename(prefix, windowsize):
+	return "%s%dx%d.hpp" % (prefix, *windowsize)
+
+
+def main_header_generate_format_iter():
+	prefixes = [HANN_PREFIX, GAUSS_FFT_PREFIX]
+
+	if GAUSS_KERNEL_GENERATE:
+		prefixes.append(GAUSS_PREFIX)
+
+	for prefix in prefixes:
+		for windowsize in WINDOW_SIZES_ROWS_COLS:
+			yield "#include \"%s\"\n" % _make_filename(prefix, windowsize)
+
+	yield "\n"
+
+
+def main_header_generate_format_savefile():
+	generated = ''.join(main_header_generate_format_iter())
+	common.append_file(generated, "MosseTables.hpp")
 
 
 def main():
+	main_header_generate_format_savefile()
+
 	if GAUSS_KERNEL_GENERATE:
 		gauss_kernel.generate_format_savefile(*common.IMSIZE_2D, common.FNAME)
 
-	for window in common.WINDOWS:
-		gauss_kernel_fft.generate_format_savefile(*window, common.FNAME)
-		hann.generate_format_savefile(*window, common.FNAME)
+	for window in WINDOW_SIZES_ROWS_COLS:
+		gauss_kernel_fft.generate_format_savefile(*window, _make_filename(GAUSS_FFT_PREFIX, window))
+		hann.generate_format_savefile(*window, _make_filename(HANN_PREFIX, window))
 
 
 if __name__ == "__main__":
