d9cafd913e89f1c677f6029b766e3c76daefa382
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-22 13:59:59 +0300

    [:common] Enh | Version for 1d

--- common.py
+++ common.py
@@ -7,17 +7,27 @@ FNAME = "mosse_constants.hpp"
 
 
 def format_array_iter(prefix, generator, nrows, ncols):
-	yield "constexpr unsigned %sHeight = %d;  // Number of rows\n" % (prefix, nrows)
-	yield "constexpr unsigned %sWidth = %d;  // Number of columns\n" % (prefix, ncols)
-	yield 'constexpr float %s[%sHeight][%sWidth] = {\n' % (prefix, prefix, prefix)
-	yield '\t{'
+	if nrows > 1:
+		yield "constexpr unsigned %sHeight = %d;  // Number of rows\n" % (prefix, nrows)
+		yield "constexpr unsigned %sWidth = %d;  // Number of columns\n" % (prefix, ncols)
+		yield 'constexpr float %s[%sHeight][%sWidth] = {\n' % (prefix, prefix, prefix)
+		yield '\t{'
 
-	for cnt, val in enumerate(generator()):
-		yield "%.4ff" % val
+		for cnt, val in enumerate(generator()):
+			yield "%.4ff" % val
 
-		if cnt % ncols == 0 and cnt != 0:
-			yield "},\n\t{"
-		else:
+			if cnt % ncols == 0 and cnt != 0:
+				yield "},\n\t{"
+			else:
+				yield ", "
+
+		yield '}\n};'
+	else:
+		yield "constexpr unsigned %sLength = %d;  //\n" % (prefix, ncols)
+		yield 'constexpr float %s[%sLength] = {' % (prefix, prefix)
+
+		for cnt, val in enumerate(generator()):
+			yield "%.4ff" % val
 			yield ", "
 
-	yield '}\n};'
+		yield '};\n'
