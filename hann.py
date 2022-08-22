7a4e22f11407f242b28aeeeae65ce002c0fdf4c4
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-22 17:22:50 +0300

    [:hann] Ref | Code decomposition

--- hann.py
+++ hann.py
@@ -9,6 +9,12 @@ import common
 ARRAY_PREFIX = "kHann"
 
 
+def make_prefix(rows, cols):
+	prefix = "%s%dx%d" % (ARRAY_PREFIX, rows, cols)
+
+	return prefix
+
+
 def generate(rows, cols):
 	generator_vert = lambda i: 0.5 * (1 - math.cos(2 * math.pi * i / (rows - 1)))
 	vec_vert = np.fromiter(map(lambda i: generator_vert(i), range(rows)), float).reshape((rows, 1))
@@ -21,10 +27,9 @@ def generate(rows, cols):
 def generate_format_iter(rows, cols):
 	yield common.CXX_NAMESPACE_BEGIN
 
-	prefix = "%s%dx%d" % (ARRAY_PREFIX, rows, cols)
 	generated = generate(rows, cols).reshape((1, rows * cols))[0]
 
-	yield ''.join(common.format_array_iter(prefix, generated, rows, cols, "float", False))
+	yield ''.join(common.format_array_iter(make_prefix(rows, cols), generated, rows, cols, "float", False))
 	yield common.CXX_NAMESPACE_END
 
 
