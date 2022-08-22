20e28b9986ba515d02a2509f77d0682de6be6e9d
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-22 13:41:49 +0300

    [:common] Impl | Added constants

--- common.py
+++ common.py
@@ -1,3 +1,10 @@
+SIGMA = 100.0
+IMSIZE_2D = (100, 100)
+IMCENTER = [i // 2 for i in IMSIZE_2D]
+PRECISION = 5
+WINDOWS = [(80, 50), (50, 80), (95, 60), (60, 95), (70, 70), (60, 60), (50, 50), (40, 40)]
+
+
 def format_array_iter(prefix, generator, nrows, ncols):
 	yield "constexpr unsigned %sHeight = %d;  // Number of rows\n" % (prefix, nrows)
 	yield "constexpr unsigned %sWidth = %d;  // Number of columns\n" % (prefix, ncols)
