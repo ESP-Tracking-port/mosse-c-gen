ef94f1f2a03cf3d7b1d4501bdac9f48632ee568c
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-22 16:05:53 +0300

    [:main] Cascade

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import hann
 
 
 def main():
+	gauss_kernel.generate_format_savefile(*common.IMSIZE_2D, common.FNAME)
+
 	for window in common.WINDOWS:
-		gauss_kernel_fft.generate_format_savefile(*window)
-		hann.generate_format_savefile(*window)
+		gauss_kernel_fft.generate_format_savefile(*window, common.FNAME)
+		hann.generate_format_savefile(*window, common.FNAME)
 
 
 if __name__ == "__main__":
