3b9a64df4ced7cba44e88f0e7aba86d776e7096d
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-01 17:04:16 +0300

    [:log_matrix] Impl

---
+++ log_matrix.py
@@ -0,0 +1,31 @@
+"""
+Generates a table of logarithms for integers from 1 to 255. Since only 8 bits
+unsigned grayscales are expected, it is faster to use a relatively small
+precompiled matrix than compute the entire logarithm. Considering that
+preprocessing is performed on each incoming frame, a substantial increase in
+performance is expected
+"""
+
+import common
+import numpy as np
+
+
+ARRAY_PREFIX = "kLogMatrix8bit"
+FILENAME = common.GEN_DIR_PREFIX + "LogMatrix.hpp"
+_LOG_INFIMUM = 1
+_LOG_SUPREMUM = 257
+
+
+def generate_format_iter():
+	yield common.CXX_NAMESPACE_BEGIN
+
+	arr = np.array(list(map(lambda i: np.log(i), range(_LOG_INFIMUM,
+		_LOG_SUPREMUM))))
+
+	yield from common.format_array_iter_nd(ARRAY_PREFIX, arr, "float")
+	yield common.CXX_NAMESPACE_END
+
+
+def generate_format_savefile():
+	for i in generate_format_iter():
+		common.append_file(i, FILENAME)
