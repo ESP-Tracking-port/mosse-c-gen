87f400c43f3fa7da85e3538f40d943b0d2fee12a
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-13 10:19:54 +0300

    [stub:main] Cascade | Dead code

--- stub/main.cpp
+++ stub/main.cpp
@@ -2,8 +2,10 @@
 
 int main(int, char **)
 {
-	auto kernelFft = Mosse::getGaussKernelFft(50, 80);
-	(void)kernelFft;
+	auto kernelFft1 = Mosse::getGaussKernelFftImReal(50, 80);
+	auto kernelFft2 = Mosse::getGaussKernelFft3d(50, 80);
+	(void)kernelFft1;
+	(void)kernelFft2;
 
 	return 0;
 }
